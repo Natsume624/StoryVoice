999b3303f20c95b798c5aa7fa23da368d2f4973d
Natsume Saku <hui723681@gmail.com>
2026-09-20 11:23:44 +0800

    fix: import Compose setContent

--- app/src/main/java/com/storyvoice/app/MainActivity.kt
+++ app/src/main/java/com/storyvoice/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.storyvoice.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.enableEdgeToEdge
